d1e0bc58c26c4fe4cab758bade48820e136983a1
Jeremy90307 <jeremy90307@gmail.com>
2024-12-29 18:44:57 +0800

    Revise the LCD code to make it clearer

--- lab10_lcd/lcd.c
+++ lab10_lcd/lcd.c
@@ -6,45 +6,36 @@ void delay(unsigned int);
 
 void main()
 {
-	write(0x38, 0);
+	write(0x38, 0);	
 	write(0x0F, 0);
 	write(0x06, 0);
 	write(0x01, 0);
 	write(0x80, 0);
 	print_msg("N16121638");
-	write(0x40, 0); // row1
+	
+	/* heart */
+	write(0x40, 0); // CGRAM addr -> the first custom character
 	write(0x00, 1);
-	write(0x41, 0);	// row2
 	write(0x0A, 1);
-	write(0x42, 0);	// row3
 	write(0x0A, 1);
-	write(0x43, 0);	// row4
 	write(0x00, 1);
-	write(0x44, 0);	// row5
 	write(0x11, 1);
-	write(0x45, 0);	// row6
 	write(0x0E, 1);
-	write(0x46, 0);	// row7
 	write(0x00, 1);
 	write(0x80 | 0x40, 0); // cursor init position => 0x80|0x40
 	write(0x00, 1);
 	
-	write(0x48, 0); // row1
+	/* heart */
+	write(0x48, 0); // CGRAM addr -> the second custom character
 	write(0x00, 1);
-	write(0x49, 0);	// row2
 	write(0x0A, 1);
-	write(0x4A, 0);	// row3
 	write(0x1F, 1);
-	write(0x4B, 0);	// row4
 	write(0x1F, 1);
-	write(0x4C, 0);	// row5
 	write(0x0E, 1);
-	write(0x4D, 0);	// row6
 	write(0x04, 1);
-	write(0x4E, 0);	// row7
 	write(0x00, 1);
 	write(0x80 | 0x41, 0); // cursor init position => 0x80|0x40
-	write(0x00, 1);
+	write(0x01, 1);
 	
 	while(1);
 }
@@ -57,11 +48,11 @@ void print_msg(char *msg)
 
 void write(char cmd, int rs_value)
 {
-	P1 = cmd;
-	P3_7 = rs_value;
-	P3_6 = 1;
+	P2 = cmd;
+	P3_4 = rs_value;
+	P3_5 = 1;
 	delay(100);
-	P3_6 = 0;
+	P3_5 = 0;
 }
 
 void delay(unsigned int i)
